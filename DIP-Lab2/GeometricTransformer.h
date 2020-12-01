78cca6f113570efbb8e4c806d3cb7308c2c98bc7
Nguyễn Tấn Thìn <ngtanthin21@gmail.com>
2020-12-01 22:25:59 +0700

    Update main.cpp, GeometricTransformer.h

--- DIP-Lab2/GeometricTransformer.h
+++ DIP-Lab2/GeometricTransformer.h
@@ -1,6 +1,11 @@
 ﻿#pragma once
 #include "opencv2/opencv.hpp"
+#include <iostream>
+using namespace std;
 using namespace cv;
+
+#define PI  3.14159265358979f
+
 /*
  Lớp base dùng để nội suy màu của 1 pixel
 */
@@ -54,10 +59,10 @@ class AffineTransform
 public:
 	void Translate(float dx, float dy);// xây dựng matrix transform cho phép tịnh tiến theo vector (dx,dy)
 	void Rotate(float angle);//xây dựng matrix transform cho phép xoay 1 góc angle
-	void Scale(float sx, float sy);//xây dựng matrix transform cho phép tỉ lệ theo hệ số 
+	void Scale(float sx, float sy);//xây dựng matrix transform cho phép tỉ lệ theo hệ số 		
 	void Symmetry(int direction, float axis);//xây dựng maxtrix transform với direction = 1 là đối xứng qua trục nganh, = 0 là qua trục đứng, axis là tọa độ trục
-	void TransformPoint(float &x, float &y);//transform 1 điểm (x,y) theo matrix transform đã có
-	
+	void TransformPoint(float& x, float& y);//transform 1 điểm (x,y) theo matrix transform đã có
+
 	AffineTransform();
 	~AffineTransform();
 };
@@ -81,9 +86,9 @@ public:
 	 - 1: Nếu biến đổi thành công
 	*/
 	int Transform(
-		const Mat &beforeImage, 
-		Mat &afterImage, 
-		AffineTransform* transformer, 
+		const Mat& beforeImage,
+		Mat& afterImage,
+		AffineTransform* transformer,
 		PixelInterpolate* interpolator);
 
 	/*
@@ -98,7 +103,7 @@ public:
 	 - 1: Nếu biến đổi thành công
 	*/
 	int RotateKeepImage(
-		const Mat &srcImage, Mat &dstImage, float angle, PixelInterpolate* interpolator);
+		const Mat& srcImage, Mat& dstImage, float angle, PixelInterpolate* interpolator);
 
 	/*
 	Hàm xoay không bảo toàn nội dung ảnh theo góc xoay cho trước
@@ -112,26 +117,26 @@ public:
 	 - 1: Nếu biến đổi thành công
 	*/
 	int RotateUnkeepImage(
-		const Mat &srcImage, Mat &dstImage, float angle, PixelInterpolate* interpolator);
+		const Mat& srcImage, Mat& dstImage, float angle, PixelInterpolate* interpolator);
 
 	/*
 	Hàm phóng to, thu nhỏ ảnh theo tỉ lệ cho trước
 	Tham số
 	- srcImage: ảnh input
-	- dstImage: ảnh sau khi thực hiện phép xoay	
-	- sx, sy: tỉ lệ phóng to, thu nhỏ ảnh	
+	- dstImage: ảnh sau khi thực hiện phép xoay
+	- sx, sy: tỉ lệ phóng to, thu nhỏ ảnh
 	- interpolator: biến chỉ định phương pháp nội suy màu
 	Trả về:
 	 - 0: Nếu ảnh input ko tồn tại hay ko thực hiện được phép biến đổi
 	 - 1: Nếu biến đổi thành công
 	*/
 	int Scale(
-		const Mat &srcImage, 
-		Mat &dstImage, 
-		float sx, float sy, 
+		const Mat& srcImage,
+		Mat& dstImage,
+		float sx, float sy,
 		PixelInterpolate* interpolator);
-		
-	
+
+
 	/*
 	Hàm thay đổi kích thước ảnh
 	Tham số
@@ -144,9 +149,9 @@ public:
 	 - 1: Nếu biến đổi thành công
 	*/
 	int Resize(
-		const Mat &srcImage, 
-		Mat &dstImage, 
-		int newWidth, int newHeight, 
+		const Mat& srcImage,
+		Mat& dstImage,
+		int newWidth, int newHeight,
 		PixelInterpolate* interpolator);
 
 	/*
@@ -161,8 +166,8 @@ public:
 	 - 1: Nếu biến đổi thành công
 	*/
 	int Flip(
-		const Mat &srcImage, 
-		Mat &dstImage, 
+		const Mat& srcImage,
+		Mat& dstImage,
 		bool direction,
 		PixelInterpolate* interpolator);
 
--- DIP-Lab2/main.cpp
+++ DIP-Lab2/main.cpp
@@ -1,13 +1,102 @@
-﻿// main.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include "GeometricTransformer.h"
 
-#include <iostream>
-#include "opencv2/opencv.hpp"
-#include "GeometricTransformer.h"
-#define PI 3.14159
-using namespace std;
-using namespace cv;
+int main(int argc, char* argv[])
+{
+	/*
+	Tham số dòng lệnh được đặt theo nguyên tắc:
+	<Program.exe> <Command> <Interpolate> <InputPath> <CmdArguments>
+	*/
 
+	// Xác định phương pháp nội suy màu
+	PixelInterpolate* interpolator;
+	if (strcmp(argv[2], "--bl") == 0)
+		interpolator = new BilinearInterpolate;
+	else if (strcmp(argv[2], "--nn") == 0)
+		interpolator = new NearestNeighborInterpolate;
+	else
+		return 0;
+
+	Mat sourceImage = imread(argv[3], IMREAD_COLOR);
+	//Mat sourceImage = imread(argv[3], IMREAD_GRAYSCALE);
+	Mat destinationImage;
+	GeometricTransformer* gmtTransform = new GeometricTransformer;
+
+	/*
+	Nếu dòng lệnh có 5 tham số thì có 3 trường hợp
+	1 - Xoay ảnh quanh tâm bảo toàn nội dung
+	2 - Xoay ảnh quanh tâm không bảo toàn nội dung
+	3 - Đối xứng ảnh qua trục đứng hay trục ngang
+	*/
+	int check = -1;
+	if (argc == 5)
+	{
+		// Xoay ảnh quanh tâm bảo toàn nội dung
+		if (strcmp(argv[1], "--rotate") == 0)
+		{
+			// Người dùng nhập vào độ -> convert từ độ sang radian 
+			float angle = atof(argv[4]) * PI / 180;
+			check = gmtTransform->RotateKeepImage(sourceImage, destinationImage, angle, interpolator);
+		}
+
+		// Xoay ảnh quanh tâm bảo toàn nội dung
+		else if (strcmp(argv[1], "--rotateN") == 0)
+		{
+			// Người dùng nhập vào độ -> convert từ độ sang radian 
+			float angle = atof(argv[4]) * PI / 180;
+			check = gmtTransform->RotateUnkeepImage(sourceImage, destinationImage, angle, interpolator);
+		}
+
+		// Đối xứng ảnh qua trục đứng hay trục ngang
+		else if (strcmp(argv[1], "--flip") == 0)
+		{
+			// Trục đối xứng
+			bool direction;
+			if (strcmp(argv[4], "Ox") == 0)
+				direction = 1;
+			else if (strcmp(argv[4], "Oy") == 0)
+				direction = 0;
+			else
+				return 0;
+
+			check = gmtTransform->Flip(sourceImage, destinationImage, direction, interpolator);
+		}
+	}
+
+	/*
+	Nếu dòng lệnh có 6 tham số thì có 2 trường hợp
+	1 - Phóng to, thu nhỏ ảnh
+	2 - Thay đổi kích thước ảnh
+	*/
+	else if (argc == 6)
+	{
+		// Phóng to, thu nhỏ ảnh
+		if (strcmp(argv[1], "--zoom") == 0)
+		{
+			check = gmtTransform->Scale(sourceImage, destinationImage, atof(argv[4]), atof(argv[5]), interpolator);
+		}
+
+		// Thay đổi kích thước ảnh
+		else if (strcmp(argv[1], "--resize") == 0)
+		{
+			check = gmtTransform->Resize(sourceImage, destinationImage, atoi(argv[4]), atoi(argv[5]), interpolator);
+		}
+	}
+
+	// Kiểm tra giá trị trả về của phép biến đổi
+	if (check == 0)
+		cout << "Bien doi that bai." << endl;
+	else if (check == 1)
+	{
+		imwrite("destinationImage.jpg", destinationImage);
+		imshow("Source Image", sourceImage);
+		imshow("Destination Image", destinationImage);
+	}
+
+	waitKey(0);
+	return 0;
+}
+
+/*
 int main()
 {
 	//Mat src = imread("flower.png");
@@ -18,7 +107,7 @@ int main()
 
 	cout << "SrcRow: " << src.rows << endl;
 	cout << "SrcCol: " << src.cols << endl;
-	
+
 	Mat dst;
 	GeometricTransformer* transformer = new GeometricTransformer();
 	//PixelInterpolate* interpolator = new NearestNeighborInterpolate();
@@ -54,4 +143,4 @@ int main()
 	cv::waitKey(0);
 	return 0;
 }
-
+*/
